831e4d379dd166ac4200e592e376c466e00c4ddf
CoolGuy <84420090+CoolGuy-0000@users.noreply.github.com>
2023-11-26 15:28:15 +0900

    Add files via upload

--- CGRandom.cpp
+++ CGRandom.cpp
@@ -3,7 +3,6 @@
 void CCGRandom::Reset(long double seed, int range) {
 	m_fSeed = seed;
 	m_iListSize = range;
-	m_iIndex = time(NULL)%m_iListSize;
 	
 	if(m_fList)free(m_fList);
 
@@ -15,18 +14,31 @@ void CCGRandom::Reset(long double seed, int range) {
 
 	this->Update(m_fSeed);
 }
-
-void CCGRandom::Update() {
-	this->Update(m_fList[time(NULL)%m_iListSize]);
-}
 void CCGRandom::Update(long double seed) {
 	m_fSeed = fabs(seed);
-	m_iIndex = time(NULL)%m_iListSize;
-	
-	float x = 0.0;
+
+	long double x = 0.0;
 
 	for (unsigned long long int i = 0; i < m_iListSize; i++) {
-		x = ((-1.0 * m_fSeed) + x) / (x + 1);
-		m_fList[i] = x;
+		x = ((-1.0 * m_fSeed) + x) / (x + 1.0);
+		m_fList[i] = fmod(x, 10.0);
 	}
+
+
+	int c;
+
+	for (unsigned long long int i = 0; i < m_iDimension; i++) {
+		
+		for (unsigned long long int k = 0; k < m_iListSize; k++) {
+			c = abs((int)(m_fList[k] * 100.0))%m_iListSize;
+			x = 0.0;
+
+			for (int m = 0; m < c; m++)
+				x = ((-1.0 * fabs(m_fList[k])) + x) / (x + 1.0);
+
+			m_fList[k] = fmod(x, 10.0);
+		}
+
+	}
+
 }
--- CGRandom.h
+++ CGRandom.h
@@ -7,14 +7,17 @@ class CCGRandom {
 public:
 	CCGRandom() {
 		m_fList = NULL;
+		m_iDimension = 0;
 		Reset(2.0, 100);
-	};
-	CCGRandom(long double seed) {
-		m_fList = NULL;
-		Reset(seed, 100);
 	};
 	CCGRandom(long double seed, int range) {
 		m_fList = NULL;
+		m_iDimension = 0;
+		Reset(seed, range);
+	};
+	CCGRandom(long double seed, int range, int dimension) {
+		m_fList = NULL;
+		m_iDimension = dimension;
 		Reset(seed, range);
 	};
 	~CCGRandom(){
@@ -22,15 +25,18 @@ public:
 	};
 
 	virtual void Reset(long double seed, int range);
-	virtual void Update();
 	virtual void Update(long double seed);
+	virtual unsigned long long int MaxIndex() { return m_iListSize; };
 	virtual long double Index(unsigned long long int i) { return m_fList[i % m_iListSize]; };
-	virtual unsigned long long int CurrentIndex() { return m_iIndex; };
-	virtual long double Get() { return this->Index(m_iIndex); };
+	virtual long double CurrentSeed() { return m_fSeed; };
+	virtual void SetDimension(unsigned long long int d) { m_iDimension = d; };
+	virtual unsigned long long int GetDimension() { return m_iDimension; };
 
 private:
+
 	unsigned long long int m_iListSize;
+	unsigned long long int m_iDimension;
 	long double* m_fList;
-	unsigned long long int m_iIndex;
 	long double m_fSeed;
+
 };
